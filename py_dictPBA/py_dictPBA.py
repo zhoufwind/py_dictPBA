5a1a7312bc905d21c6ceabeb5f26c5e90c06e6f4
Stephen Zhou <zhoufwind@hotmail.com>
2014-06-01 22:56:08 -0700

    Update: using get() function

--- py_dictPBA/py_dictPBA.py
+++ py_dictPBA/py_dictPBA.py
@@ -28,9 +28,18 @@ name = raw_input('Name: ')
 
 # choose function
 request = raw_input('phone number(p) or address(a)?')
+
+key = request # input either 'p' nor 'a'
 if request == 'p': key = 'phone'
 if request == 'a': key = 'addr'
 
 # print the value
-if name in people: print "%s's %s is: \'%s\'." % (name, labels[key], people[name][key])
+# if name in people: print "%s's %s is: \'%s\'." % (name, labels[key], people[name][key])
+
+# using get() get default value
+person = people.get(name, {})
+label = labels.get(key, key)
+result = person.get(key, 'not available')
+
+print "%s's %s is: \'%s\'." % (name, label, result)
 
